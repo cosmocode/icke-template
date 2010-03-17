6d7b9bcbf217bf3fc399a4dc28716c1eb542653d
root <root@ickewiki.de>
2010-03-17 09:53:03 +0100

    Fix quicksearch

--- script.js
+++ script.js
@@ -4,18 +4,14 @@ addInitEvent(function(){
      */
     jQuery("#icke__sidebar .search .namespace").replaceWith(jQuery('<input class="namespace" type="hidden" name="namespace" value="" />'));
     jQuery("#icke__sidebar .search #ns_custom").show();
-    jQuery(document).live("click", function(){
-        jQuery("#icke__sidebar .search #ns_custom:not(.closed)").addClass("closed");
-    });
-    jQuery("#icke__sidebar .search #ns_custom.closed").live("click", function(){
-        jQuery(this).removeClass("closed");
+    jQuery("#icke__sidebar .search #ns_custom").live("click", function(){
+        jQuery(this).toggleClass("closed");
     });
     jQuery("#icke__sidebar .search #ns_custom:not(.closed) li").live("click", function(){
-        jQuery("#icke__sidebar .search .namespace").val(jQuery(this).attr("class"));
+        jQuery("#icke__sidebar .search .namespace").val(jQuery(this).attr("class").match(/(?:(\w+)_search|^()$)/)[1]);
         jQuery(this).parent().animate({'top': (jQuery(this).prevAll().size()*-30) + 'px' },"slow");
         jQuery(this).parent().parent().addClass("closed");
     });
-
     
     /** hover effect for ie6 **/
     if (jQuery.browser === 'msie' && jQuery.browser.version === '6.0') {
