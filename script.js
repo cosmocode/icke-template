b5d02653f9c892f936ee789c714e16129b348b0e
root <root@ickewiki.de>
2010-03-17 11:53:22 +0100

    Add do css, do not underline do action links

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ addInitEvent(function(){
     });
     
     /** hover effect for ie6 **/
-    if (jQuery.browser === 'msie' && jQuery.browser.version === '6.0') {
+    if (jQuery.browser == 'msie') {
         jQuery(document).ready(function(){
             jQuery("#icke__quicknav li").hover(
                 function() { jQuery(this).addClass("hover"); },
