f69711ffa0b9531f60ef53ade8c7df48e1e59724
root <root@ickewiki.de>
2010-03-17 15:35:13 +0100

    Fix IE popouts

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ addInitEvent(function(){
     });
     
     /** hover effect for ie6 **/
-    if (jQuery.browser == 'msie') {
+    if (jQuery.browser.msie) {
         jQuery(document).ready(function(){
             jQuery("#icke__quicknav li").hover(
                 function() { jQuery(this).addClass("hover"); },
