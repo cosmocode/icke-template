46f1515db2c895c2264b0eca5921194db1806b23
root <root@ickewiki.de>
2010-06-14 11:58:35 +0200

    Make IE6 popout fix work with jQuery 1.4.2

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 addInitEvent(function(){
     /** hover effect for ie6 **/
-    if (jQuery.browser.msie) {
-        jQuery(document).ready(function(){
-            jQuery("#icke__quicknav li").hover(
-                function() { jQuery(this).addClass("hover"); },
-                function() { jQuery(this).removeClass("hover"); }
-            );
-        });
-    }
+    jQuery(document).ready(function(){
+        jQuery("#icke__quicknav li").hover(
+            function() { jQuery(this).addClass("hover"); },
+            function() { jQuery(this).removeClass("hover"); }
+        );
+    });
 });
 
