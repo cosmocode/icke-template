7e56bb1d7337bb304fa01e0eff7da241c187a422
Adrian Lang <lang@cosmocode.de>
2010-08-20 18:03:30 +0200

    Add profile link to the tools popup

--- tools.php
+++ tools.php
@@ -4,5 +4,6 @@ $icke_tools = array(
   array('type' => 'action', 'value' => 'recent'),
   array('type' => 'action', 'value' => 'index'),
   array('type' => 'action', 'value' => 'subscribe'),
-  array('type' => 'action', 'value' => 'admin')
+  array('type' => 'action', 'value' => 'admin'),
+  array('type' => 'action', 'value' => 'profile')
 );
