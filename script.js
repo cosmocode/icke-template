a20e38b631d95ef7ffef2a98bd3c1490a40c8f5f
Adrian Lang <lang@cosmocode.de>
2010-11-30 10:50:11 +0100

    Make template usable without jQuery

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 addInitEvent(function(){
-    /** hover effect for ie6 **/
-    jQuery("#icke__quicknav li").hover(
-        function() { jQuery(this).addClass("hover"); },
-        function() { jQuery(this).removeClass("hover"); }
-    );
+    if (jQuery) {
+        /** hover effect for ie6 **/
+        jQuery("#icke__quicknav li").hover(
+            function() { jQuery(this).addClass("hover"); },
+            function() { jQuery(this).removeClass("hover"); }
+        );
+    }
 });
 
