7e97aabda54d4afd4a4ee919dad3ada2f347e3fd
Andreas Gohr <gohr@cosmocode.de>
2012-01-30 15:15:48 +0100

    collapse sections in mobile style (wikipedia-like)

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-addInitEvent(function(){
-    if (jQuery) {
-        /** hover effect for ie6 **/
-        jQuery("#icke__quicknav li").hover(
-            function() { jQuery(this).addClass("hover"); },
-            function() { jQuery(this).removeClass("hover"); }
-        );
-    }
-});
+jQuery(function(){
 
+    /** hover effect for ie6 **/
+    jQuery("#icke__quicknav li").hover(
+        function() { jQuery(this).addClass("hover"); },
+        function() { jQuery(this).removeClass("hover"); }
+    );
+
+    if(jQuery(window).width() <= 600){
+        for(i=2; i<6; i++){
+            jQuery('#icke__page div.level'+i).hide();
+            jQuery('#icke__page h'+i).click(function(){
+                jQuery(this).next('div').toggle();
+            });
+        }
+    }
+
+});
