8742489cfafaccd8b6881883921761f02495de89
Michael Große <grosse@cosmocode.de>
2017-07-19 12:54:05 +0200

    Fix whitespace and other pure style-issues etc.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-jQuery(function(){
-
+jQuery(function () {
     /** hover effect for ie6 **/
-    jQuery("#icke__quicknav li").hover(
-        function() { jQuery(this).addClass("hover"); },
-        function() { jQuery(this).removeClass("hover"); }
+    jQuery('#icke__quicknav li').hover(
+        function () {
+            jQuery(this).addClass('hover');
+        },
+        function () {
+            jQuery(this).removeClass('hover');
+        }
     );
 
-    if(jQuery(window).width() <= 600){
-        for(i=2; i<6; i++){
-            jQuery('#icke__page div.level'+i).hide();
-            jQuery('#icke__page h'+i).click(function(){
+    if (jQuery(window).width() <= 600) {
+        for (i = 2; i < 6; i++) {
+            jQuery('#icke__page div.level' + i).hide();
+            jQuery('#icke__page h' + i).click(function () {
                 jQuery(this).next('div').toggle();
             });
         }
@@ -18,7 +21,7 @@ jQuery(function(){
     getULwidth = function getULwidth($ul) {
         var width = 0;
         var $li = $ul.children('li');
-        $li.each(function() {
+        $li.each(function () {
             var $div = jQuery(this).children('div.li').first();
             var content = $div.html();
             $div.html('<span id="widthMeasure">' + content + '</span>');
@@ -30,7 +33,7 @@ jQuery(function(){
         return parseInt(width);
     };
 
-    jQuery('div.sec_level').css('display','inline');
+    jQuery('div.sec_level').css('display', 'inline');
     while (jQuery('.popup_content ul').children('li.node').length) {
         var $currentNode = jQuery('.popup_content ul').children('li.node').first();
         var $newPopout = $currentNode.closest('div.sec_level').clone();
@@ -38,11 +41,11 @@ jQuery(function(){
 
         var newZIndex = $currentNode.closest('div.sec_level').first().zIndex() + 5;
         var parentWidth = getULwidth($currentNode.closest('ul'));
-        var parentPaddingLeft = parseInt($currentNode.css('padding-left').replace("px", ""));
-        var parentPaddingRight = parseInt($currentNode.css('padding-right').replace("px", ""));
+        var parentPaddingLeft = parseInt($currentNode.css('padding-left').replace('px', ''));
+        var parentPaddingRight = parseInt($currentNode.css('padding-right').replace('px', ''));
         var newCSS = {
             'z-index': newZIndex,
-            'left' : parentPaddingLeft + parentWidth + parentPaddingRight + 'px'
+            left: parentPaddingLeft + parentWidth + parentPaddingRight + 'px',
         };
         $newPopout.css(newCSS);
         $currentNode.children('ul').first().appendTo($newPopout);
@@ -50,6 +53,5 @@ jQuery(function(){
         $currentNode.removeClass('node');
         $currentNode.addClass('navNode');
     }
-    jQuery('div.sec_level').css('display','');
-
+    jQuery('div.sec_level').css('display', '');
 });
