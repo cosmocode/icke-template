11ec36a3101e71b14b2ba364bd214cad7708ee92
Michael Große <grosse@cosmocode.de>
2017-07-26 14:47:18 +0200

    Use HTML5 doctype

--- main.php
+++ main.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <?php
 require_once DOKU_TPLINC.'functions.php';
 require_once DOKU_TPLINC.'components.php';
