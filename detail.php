108717c7fa1febb07c1d136a9c64496f5abf848b
Michael Große <grosse@cosmocode.de>
2017-09-11 16:41:23 +0200

    fix: declare the page as HTML 5

--- detail.php
+++ detail.php
@@ -7,8 +7,7 @@ require_once DOKU_TPLINC.'functions.php';
 require_once DOKU_TPLINC.'components.php';
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $conf['lang']?>" lang="<?php echo $conf['lang']?>" dir="ltr">
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
